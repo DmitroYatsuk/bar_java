5a9ed1d96ccb4dd7dc78863ecaf97e712f561989
DmitroYatsuk <dyatsuk@gmail.com>
2016-08-23 00:37:58 +0300

    Group counter has been added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupCreationTests extends TestBase{
+public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+    @Test
+    public void testGroupCreation() {
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
+        app.getGroupHelper().createGroup(new GroupData("group1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
 
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("group1", null, null));
-  }
+    }
 
 }
