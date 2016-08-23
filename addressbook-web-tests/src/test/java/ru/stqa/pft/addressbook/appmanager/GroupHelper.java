390737a3519152b3b6dc99a7bb2085ec83a4d25f
DmitroYatsuk <dyatsuk@gmail.com>
2016-08-24 00:34:18 +0300

    Group toString and equals/hashcode have been added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,66 +2,81 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kiro on 12.08.16.
  */
 public class GroupHelper extends BaseHelper {
 
-  public GroupHelper(WebDriver wd) {
-    super(wd);
-  }
-
-  public void returnToGroupPage() {
-    click(By.linkText("group page"));
-  }
-
-  public void submitGroupCreation() {
-    click(By.name("submit"));
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    type(By.name("group_name"), groupData.getName());
-    type(By.name("group_header"), groupData.getHeader());
-    type(By.name("group_footer"), groupData.getFooter());
-    click(By.xpath("//div[@id='content']/form"));
-  }
-
-  public void initGroupCreation() {
-    click(By.name("new"));
-  }
-
-  public void selectGroup(int index) {
-    if (!wd.findElement(By.name("selected[]")).isSelected()) {
-      wd.findElements(By.name("selected[]")).get(index).click();
+    public GroupHelper(WebDriver wd) {
+        super(wd);
     }
-  }
 
-  public void deleteSelectedGroups() {
-    click(By.name("delete"));
-  }
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
+    }
 
-  public void initGroupModification() {
-    click(By.name("edit"));
-  }
+    public void submitGroupCreation() {
+        click(By.name("submit"));
+    }
 
-  public void submitGroupModification() {
-    click(By.name("update"));
-  }
+    public void fillGroupForm(GroupData groupData) {
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+        click(By.xpath("//div[@id='content']/form"));
+    }
 
-  public void createGroup(GroupData group) {
-    initGroupCreation();
-    fillGroupForm(group);
-    submitGroupCreation();
-    returnToGroupPage();
-  }
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
 
-  public boolean isThereAGroup() {
-    return isElementPresent(By.name("selected[]"));
-  }
+    public void selectGroup(int index) {
+        if (!wd.findElement(By.name("selected[]")).isSelected()) {
+            wd.findElements(By.name("selected[]")).get(index).click();
+        }
+    }
 
-  public int getGroupCount() {
-    return wd.findElements(By.name("selected[]")).size();
-  }
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
+
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModification() {
+        click(By.name("update"));
+    }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+    }
+
+    public int getGroupCount() {
+        return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) {
+            String name = element.getText();
+            groups.add(new GroupData(name, null, null));
+        }
+
+        return groups;
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by Kiro on 15.08.16.
  */
@@ -15,13 +17,15 @@ public class GroupModificationTests extends TestBase{
     if (!app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test", null, null));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test3", "test2", "test1"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
+
     Assert.assertEquals(after, before);
   }
 }
