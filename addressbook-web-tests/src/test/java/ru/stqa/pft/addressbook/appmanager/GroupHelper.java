099bd98169f0de4fc50555ebf77629d392b114eb
DmitroYatsuk <dyatsuk@gmail.com>
2016-09-11 16:28:20 +0300

    Group Cache creation  has been added.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -51,12 +51,6 @@ public class GroupHelper extends BaseHelper {
     returnToGroupPage();
   }
 
-  public void delete(GroupData group) {
-    selectGroupById(group.getId());
-    deleteSelectedGroups();
-    returnToGroupPage();
-  }
-
   private void selectGroupById(int id) {
     wd.findElement(By.cssSelector("input[value='" + id +"']")).click();
   }
@@ -77,6 +71,7 @@ public class GroupHelper extends BaseHelper {
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -85,6 +80,14 @@ public class GroupHelper extends BaseHelper {
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
+    groupCache = null;
+    returnToGroupPage();
+  }
+
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
+    deleteSelectedGroups();
+    groupCache = null;
     returnToGroupPage();
   }
 
@@ -107,15 +110,20 @@ public class GroupHelper extends BaseHelper {
     return groups;
   }
 
+  private Groups groupCache = null;
+
   public Groups all() {
-    Groups groups = new Groups();
+    if(groupCache != null){
+      return new Groups(groupCache);
+    }
+    groupCache = new Groups();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
-      groups.add(new GroupData().withId(id).withName(name));
+      groupCache.add(new GroupData().withId(id).withName(name));
     }
-    return  groups;
+    return new Groups(groupCache);
   }
  }
 
