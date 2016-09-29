3d713ea85c9ad538b249b8b8abc0a0927d1fa108
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2016-09-29 12:12:28 +0300

    This commit has been done from IDEA. Json data generator and data provider have been created.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -46,9 +48,19 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
     } else {
       System.out.println("Unrecognized format " + format);
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -60,15 +72,6 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private List<GroupData> generateGroups(int count) {
-    List<GroupData> groups = new ArrayList<>();
-    for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withName(String.format("test %s", i))
-              .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
-    }
-    return groups;
-  }
-
   private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
@@ -77,5 +80,13 @@ public class GroupDataGenerator {
     writer.close();
   }
 
+  private List<GroupData> generateGroups(int count) {
+    List<GroupData> groups = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData().withName(String.format("test %s", i))
+              .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
+    }
+    return groups;
+  }
 
 }
