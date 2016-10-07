7a9968d93bf28d711dc8468fe28eeec454bcd7a2
DmitroYatsuk <dmytro.iatsuk@gmail.com>
2016-10-07 15:45:51 +0300

    This commit has been done from IDEA. Hibernate connectivity to DB for Contacts and Groups has been created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -26,35 +26,34 @@ public class HbConnectionTest {
             .configure() // configures settings from hibernate.cfg.xml
             .build();
     try {
-      sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
-    }
-    catch (Exception e) {
+      sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    } catch (Exception e) {
       e.printStackTrace();
       // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
       // so destroy it manually.
-      StandardServiceRegistryBuilder.destroy( registry );
+      StandardServiceRegistryBuilder.destroy(registry);
     }
   }
 
   @Test
-  public void testHbConnectionForContacts(){
+  public void testHbConnectionForContacts() {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<ContactData> result = session.createQuery( "from ContactData where deprecated = 0" ).list();
-    for (ContactData contact : result ) {
-      System.out.println( contact );
+    List<ContactData> result = session.createQuery("from ContactData where deprecated = '0000-00-00'").list();
+    for (ContactData contact : result) {
+      System.out.println(contact);
     }
     session.getTransaction().commit();
     session.close();
   }
 
   @Test
-  public void testHbConnectionForGroups(){
+  public void testHbConnectionForGroups() {
     Session session = sessionFactory.openSession();
     session.beginTransaction();
-    List<GroupData> result = session.createQuery( "from GroupData where deprecated = 0" ).list();
-    for (GroupData group : result ) {
-      System.out.println( group );
+    List<GroupData> result = session.createQuery("from GroupData where deprecated = '0000-00-00'").list();
+    for (GroupData group : result) {
+      System.out.println(group);
     }
     session.getTransaction().commit();
     session.close();
